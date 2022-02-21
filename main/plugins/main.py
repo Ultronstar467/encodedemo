73f6c0e57dff363972e79ce041ddeee709a65555
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-02-21 21:33:44 +0530

    Update main.py

--- main/plugins/main.py
+++ main/plugins/main.py
@@ -353,7 +353,7 @@ async def _480(event):
     if not os.path.isdir("encodemedia"):
         await event.delete()
         os.mkdir("encodemedia")
-        cmd = '-vf scale=854x480 -b:v 750k -quality good -speed 4 -crf 30'
+        cmd = '-vf scale=854x480 -b:v 750k -quality good -speed 4 -crf 0'
         await encode(event, msg, cmd)
         os.rmdir("encodemedia")
     else:
@@ -369,7 +369,7 @@ async def _360(event):
     if not os.path.isdir("encodemedia"):
         await event.delete()
         os.mkdir("encodemedia")
-        cmd = '360'
+        cmd = '-vf scale=640x360 -b:v 276k -quality good -speed 4 -crf 0'
         await encode(event, msg, cmd)
         os.rmdir("encodemedia")
     else:
@@ -385,7 +385,7 @@ async def _240(event):
     if not os.path.isdir("encodemedia"):
         await event.delete()
         os.mkdir("encodemedia")
-        cmd = '240'
+        cmd = '-vf scale=320x240 -b:v 196k -quality good -speed 4 -crf 0'
         await encode(event, msg, cmd)
         os.rmdir("encodemedia")
     else:
@@ -403,7 +403,7 @@ async def _720(event):
     if not os.path.isdir("encodemedia"):
         await event.delete()
         os.mkdir("encodemedia")
-        cmd = '720'
+        cmd = '-vf scale=1280x720 -b:v 1024k -quality good -speed 4 -crf 0'
         await encode(event, msg, cmd)
         os.rmdir("encodemedia")
     else:
