340d671c356e2942353a86ee4f7788cd09cf770a
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-02-21 20:11:36 +0530

    Update encoder.py

--- main/plugins/encoder.py
+++ main/plugins/encoder.py
@@ -1,1 +1,116 @@
+async def compress(event, msg, ffmpeg_cmd, ps_name=None):
+    if ps_name is None:
+        ps_name = '**ENCODING:**'
+    Drone = event.client
+    edit = await Drone.send_message(event.chat_id, "Trying to process.", reply_to=msg.id)
+    new_name = "out_" + dt.now().isoformat("_", "seconds")
+    if hasattr(msg.media, "document"):
+        file = msg.media.document
+    else:
+        file = msg.media
+    mime = msg.file.mime_type
+    if 'mp4' in mime:
+        n = "media_" + dt.now().isoformat("_", "seconds") + ".mp4"
+        out = new_name + ".mp4"
+    elif msg.video:
+        n = "media_" + dt.now().isoformat("_", "seconds") + ".mp4"
+        out = new_name + ".mp4"
+    elif 'x-matroska' in mime:
+        n = "media_" + dt.now().isoformat("_", "seconds") + ".mkv" 
+        out = new_name + ".mp4"            
+    elif 'webm' in mime:
+        n = "media_" + dt.now().isoformat("_", "seconds") + ".webm" 
+        out = new_name + ".mp4"
+    else:
+        n = msg.file.name
+        ext = (n.split("."))[1]
+        out = new_name + ext
+    DT = time.time()
+    _ps = "ENCODE"
+    if ps_name != "**ENCODING:**":
+        _ps = "ENCODE"
+    log = await LOG_START(event, f'**{str(_ps)} PROCESS STARTED**\n\n[Bot is busy now]({SUPPORT_LINK})')
+    log_end_text = f'**{_ps} PROCESS FINISHED**\n\n[Bot is free now]({SUPPORT_LINK})'
+    try:
+        await fast_download(n, file, Drone, edit, DT, "**DOWNLOADING:**")
+    except Exception as e:
+        os.rmdir("encodemedia")
+        await log.delete()
+        await LOG_END(event, log_end_text)
+        print(e)
+        return await edit.edit(f"An error occured while downloading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False) 
+    name = '__' + dt.now().isoformat("_", "seconds") + ".mp4"
+    os.rename(n, name)
+    FT = time.time()
+    progress = f"progress-{FT}.txt"
+    cmd = f'ffmpeg -hide_banner -loglevel quiet -progress {progress} -i """{name}""" {str(ffmpeg_cmd)} """{out}""" -y'
+    try:
+        await ffmpeg_progress(cmd, name, progress, FT, edit, ps_name, log=log)
+    except Exception as e:
+        await log.delete()
+        await LOG_END(event, log_end_text)
+        os.rmdir("encodemedia")
+        print(e)
+        return await edit.edit(f"An error occured while FFMPEG progress.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)  
+    out2 = dt.now().isoformat("_", "seconds") + ".mp4" 
+    if msg.file.name:
+        out2 = msg.file.name
+    else:
+        out2 = dt.now().isoformat("_", "seconds") + ".mp4" 
+    os.rename(out, out2)
+    i_size = os.path.getsize(name)
+    f_size = os.path.getsize(out2)     
+    text = F'**ENCODED by:** @{BOT_UN}'
+    if ps_name != "**ENCODING:**":
+        text = f'**ENCODED by** : @{BOT_UN}\n\nbefore compressing : `{i_size}`\nafter compressing : `{f_size}`'
+    UT = time.time()
+    await log.edit("Uploading file.")
+    if 'x-matroska' in mime:
+        try:
+            uploader = await fast_upload(f'{out2}', f'{out2}', UT, Drone, edit, '**UPLOADING:**')
+            await Drone.send_file(event.chat_id, uploader, caption=text, thumb=JPG, force_document=True)
+        except Exception as e:
+            await log.delete()
+            await LOG_END(event, log_end_text)
+            os.rmdir("encodemedia")
+            print(e)
+            return await edit.edit(f"An error occured while uploading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)
+    elif 'webm' in mime:
+        try:
+            uploader = await fast_upload(f'{out2}', f'{out2}', UT, Drone, edit, '**UPLOADING:**')
+            await Drone.send_file(event.chat_id, uploader, caption=text, thumb=JPG, force_document=True)
+        except Exception as e:
+            await log.delete()
+            await LOG_END(event, log_end_text)
+            os.rmdir("encodemedia")
+            print(e)
+            return await edit.edit(f"An error occured while uploading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)
+    else:
+        metadata = video_metadata(out2)
+        width = metadata["width"]
+        height = metadata["height"]
+        duration = metadata["duration"]
+        attributes = [DocumentAttributeVideo(duration=duration, w=width, h=height, supports_streaming=True)]
+        try:
+            uploader = await fast_upload(f'{out2}', f'{out2}', UT, Drone, edit, '**UPLOADING:**')
+            await Drone.send_file(event.chat_id, uploader, caption=text, thumb=JPG3, attributes=attributes, force_document=False)
+        except Exception:
+            try:
+                uploader = await fast_upload(f'{out2}', f'{out2}', UT, Drone, edit, '**UPLOADING:**')
+                await Drone.send_file(event.chat_id, uploader, caption=text, thumb=JPG, force_document=True)
+            except Exception as e:
+                await log.delete()
+                await LOG_END(event, log_end_text)
+                os.rmdir("encodemedia")
+                print(e)
+                return await edit.edit(f"An error occured while uploading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)
+    await edit.delete()
+    os.remove(name)
+    os.remove(out2)
+    await log.delete()
+    log_end_text2 = f'**{_ps} PROCESS FINISHED**\n\nTime Taken: {round((time.time()-DT)/60)} minutes\nInitial size: {i_size/1000000}mb.\nFinal size: {f_size/1000000}mb.\n\n[Bot is free now.]({SUPPORT_LINK})'
+    await LOG_END(event, log_end_text2)
+    
+
+
 
