7bd77794da4efbbd84d50debe730a1dab9c0a785
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-02-22 10:08:31 +0530

    Update main.py

--- main/plugins/main.py
+++ main/plugins/main.py
@@ -357,7 +357,7 @@ async def _480(event):
     if not os.path.isdir("encodemedia"):
         await event.delete()
         os.mkdir("encodemedia")
-        cmd = '-vcodec libx265 -crf 28 -acodec copy -vf "scale=854:480" -preset medium'
+        cmd = '-vcodec libx265 -crf 28 -acodec copy -vf "scale=854:480" -preset veryslow'
         await encode(event, msg, cmd)
         os.rmdir("encodemedia")
     else:
@@ -373,7 +373,7 @@ async def _360(event):
     if not os.path.isdir("encodemedia"):
         await event.delete()
         os.mkdir("encodemedia")
-        cmd = '-vcodec libx265 -crf 28 -acodec copy -vf "scale=640:360" -preset medium'
+        cmd = '-vcodec libx265 -crf 28 -acodec copy -vf "scale=640:360" -preset veryslow'
         await encode(event, msg, cmd)
         os.rmdir("encodemedia")
     else:
@@ -389,7 +389,7 @@ async def _240(event):
     if not os.path.isdir("encodemedia"):
         await event.delete()
         os.mkdir("encodemedia")
-        cmd = '-vcodec libx265 -crf 28 -acodec copy -vf "scale=320:240" -preset medium'
+        cmd = '-vcodec libx265 -crf 28 -acodec copy -vf "scale=320:240" -preset veryslow'
         await encode(event, msg, cmd)
         os.rmdir("encodemedia")
     else:
@@ -405,7 +405,7 @@ async def _540(event):
     if not os.path.isdir("encodemedia"):
         await event.delete()
         os.mkdir("encodemedia")
-        cmd = '-vcodec libx265 -crf 28 -acodec copy -vf "scale=1024:576" -preset medium'
+        cmd = '-vcodec libx265 -crf 28 -acodec copy -vf "scale=1024:576" -preset veryslow'
         await encode(event, msg, cmd)
         os.rmdir("encodemedia")
     else:
@@ -422,7 +422,7 @@ async def _1080(event):
     if not os.path.isdir("encodemedia"):
         await event.delete()
         os.mkdir("encodemedia")
-        cmd = '-vcodec libx265 -crf 28 -acodec copy -vf "scale=1920:1080" -preset medium'
+        cmd = '-vcodec libx265 -crf 28 -acodec copy -vf "scale=1920:1080" -preset veryslow'
         await encode(event, msg, cmd)
         os.rmdir("encodemedia")
     else:
@@ -439,7 +439,7 @@ async def _720(event):
     if not os.path.isdir("encodemedia"):
         await event.delete()
         os.mkdir("encodemedia")
-        cmd = '-vcodec libx265 -crf 28 -acodec copy -vf "scale=1280:720" -preset medium'
+        cmd = '-vcodec libx265 -crf 28 -acodec copy -vf "scale=1280:720" -preset veryslow'
         await encode(event, msg, cmd)
         os.rmdir("encodemedia")
     else:
