b4e2761bbdf5df32a5e2f91237627e6689c85381
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-02-23 11:22:07 +0530

    Update main.py

--- main/plugins/main.py
+++ main/plugins/main.py
@@ -403,7 +403,7 @@ async def _240(event):
     if not os.path.isdir("compressmedia"):
         await event.delete()
         os.mkdir("compressmedia")
-        cmd = '-vcodec libx264 -crf 28 -acodec copy -vf "scale=320:240" -preset ultrafast'
+        cmd = '-vcodec libx264 -crf 30 -acodec copy -vf "scale=320:240" -preset ultrafast'
         await compress(event, msg, cmd)
         os.rmdir("compressmedia")
     else:
@@ -419,7 +419,7 @@ async def _360(event):
     if not os.path.isdir("compressmedia"):
         await event.delete()
         os.mkdir("compressmedia")
-        cmd = '-vcodec libx264 -crf 28 -acodec copy -vf "scale=640:360" -preset ultrafast'
+        cmd = '-vcodec libx264 -crf 30 -acodec copy -vf "scale=640:360" -preset ultrafast'
         await compress(event, msg, cmd)
         os.rmdir("compressmedia")
     else:
@@ -435,7 +435,7 @@ async def _480(event):
     if not os.path.isdir("compressmedia"):
         await event.delete()
         os.mkdir("compressmedia")
-        cmd = '-vcodec libx264 -crf 28 -acodec copy -vf "scale=854:480" -preset ultrafast'
+        cmd = '-vcodec libx264 -crf 30 -acodec copy -vf "scale=854:480" -preset ultrafast'
         await compress(event, msg, cmd)
         os.rmdir("compressmedia")
     else:
@@ -451,7 +451,7 @@ async def _540(event):
     if not os.path.isdir("compressmedia"):
         await event.delete()
         os.mkdir("compressmedia")
-        cmd = '-vcodec libx264 -crf 28 -acodec copy -vf "scale=854:480" -preset ultrafast'
+        cmd = '-vcodec libx264 -crf 30 -acodec copy -vf "scale=854:480" -preset ultrafast'
         await compress(event, msg, cmd)
         os.rmdir("compressmedia")
     else:
