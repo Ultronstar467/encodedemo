5e3d046da864b22557a301c798e235f41f9d71d9
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-03-14 09:48:10 +0530

    Update main.py

--- main/plugins/main.py
+++ main/plugins/main.py
@@ -373,7 +373,7 @@ async def _240(event):
     if not os.path.isdir("encodemedia"):
         await event.delete()
         os.mkdir("encodemedia")
-        cmd = '-vf scale=320x240 -b:v -quality realtime -speed 8 -crf 28'
+        cmd = '-c:v libx264 -pix_fmt yuv420p -preset ultrafast -s 426x240 -crf 18 -c:a libopus -ac 2 -ab 128k -c:s copy'
         await encode(event, msg, cmd)
         os.rmdir("encodemedia")
     else:
