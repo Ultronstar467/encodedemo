17ea29ab3c60520867c380aae86d0206b511e222
Naysa Bots <96517910+Naysabots@users.noreply.github.com>
2022-03-15 10:30:56 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-echo "Cloning Repo...."
-git clone https://github.com/Tellybots/ConvertorBotPro /ConvertorBotPro
-cd /ConvertorBotPro
-pip3 install -U -r requirements.txt
-echo "Starting Bot.... Please Wait. Check You Log. Bot Credit @DKBOTZ\n Special Credit :- @Tellybots"
-python3 bot.py
-
