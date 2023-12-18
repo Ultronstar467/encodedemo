21f7c6e6494b7107a3d5f71e7a1549316af34a67
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2023-12-18 17:55:41 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -9,6 +9,7 @@ from LOCAL.localisation import JPG0 as file
 from LOCAL.localisation import info_text, spam_notice, help_text, DEV, source_text, SUPPORT_LINK
 from ethon.teleutils import mention
 from ethon.mystarts import vc_menu
+from main.plugins.actions import heroku_restart
 
 @Drone.on(events.NewMessage(incoming=True, pattern="/start"))
 async def start(event):
